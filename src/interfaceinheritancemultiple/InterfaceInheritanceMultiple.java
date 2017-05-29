dbec69c52f923364a312188d5a8ee28261cf379e
tiago.lucas <brother_87_22@hotmail.com>
2017-05-29 17:47:48 -0300

    versão 1

---
+++ src/interfaceinheritancemultiple/InterfaceInheritanceMultiple.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceinheritancemultiple;
+
+/**
+ *
+ * @author tiago.lucas
+ */
+public class InterfaceInheritanceMultiple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        C c1 = new D();
+        c1.showA();
+        c1.showB();
+        c1.showC();
+    }
+    
+}
